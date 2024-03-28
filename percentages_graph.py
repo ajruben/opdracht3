caad5591421ccc47bb6accc2099ac320c039c1e2
bramwolk <160227089+bramwolk@users.noreply.github.com>
2024-03-28 10:30:57 +0100

    Update percentages_graph.py

--- percentages_graph.py
+++ percentages_graph.py
@@ -7,21 +7,23 @@ df = pd.read_csv("37296ned_UntypedDataSet_26032024_091942.csv", header = 0, sep=
 df['Perioden'] = pd.to_datetime(df['Perioden'].str[:4], format='%Y')
 
 #setting divorced, unmarried, married and widowed people in the Netherlands as a percentage of the total population.
-df["percentage_divorced"] = df["TotaleBevolking_4"] / df["Gescheiden_8"] * 100
-df["percentage_divorced"] = df["percentage_divorced"].round(2)
-df["percentage_unmarried"] = df["TotaleBevolking_4"] / df["Ongehuwd_5"] * 100
-df["percentage_unmarried"] = df["percentage_unmarried"].round(2)
-df["percentage_married"] = df["TotaleBevolking_4"] / df["Gehuwd_6"] * 100
-df["percentage_married"] = df["percentage_married"].round(2)
-df["percentage_widowed"] = df["TotaleBevolking_4"] / df["Verweduwd_7"] * 100
-df["percentage_widowed"] = df["percentage_widowed"].round(2)
+df["Percentage_Divorced"] = df["Gescheiden_8"] / df["TotaleBevolking_4"] * 100
+df["Percentage_Divorced"] = df["Percentage_Divorced"].round(2)
+df["Percentage_NeverMarried"] = df["Ongehuwd_5"] / df["TotaleBevolking_4"] * 100
+df["Percentage_NeverMarried"] = df["Percentage_NeverMarried"].round(2)
+df["Percentage_Married"] = df["Gehuwd_6"] / df["TotaleBevolking_4"] * 100
+df["Percentage_Married"] = df["Percentage_Married"].round(2)
+df["Percentage_Widowed"] = df["Verweduwd_7"] / df["TotaleBevolking_4"] * 100
+df["Percentage_Widowed"] = df["Percentage_Widowed"].round(2)
+
+print(df)
 
 #plot the percentage of divorced people in the Netherlands over time. 
 df.set_index('Perioden', inplace=True)
-plt.plot(df.index, df["percentage_divorced", "percentage_unmarried", "percentage_married", "percentage_widowed"], marker='o', linestyle='-')
-plt.title('PercentagesOver Time')
+plt.plot(x=df.index, y=df["Percentage_Divorced", "Percentage_NeverMarried", "Percentage_Married", "Percentage_Widowed"], marker='o', linestyle='-')
+plt.title('Percentages Over Time')
 plt.xlabel('Year')
-plt.ylabel("% of dutch population")
-plt.legend("percentage divorced", "percentage unmarried", "percentage married", "percentage widowed")
+plt.ylabel("% of Dutch Population")
+plt.legend("Percentage Divorced", "Percentage Unmarried", "Percentage Married", "Percentage Widowed")
 plt.grid(True)
 plt.show()
